99726d676f0d1bba06eac6dea838ac55d674c083
zhaoyiwu <1508108406@qq.com>
2017-12-28 12:51:53 +0800

    init project

---
+++ src/main/java/com/simon/leetcode/SimonApplication.java
@@ -0,0 +1,12 @@
+package com.simon.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimonApplication.class, args);
+	}
+}
---
+++ src/main/java/com/simon/leetcode/list/linkedlist/MergeTwoLists.java
@@ -0,0 +1,21 @@
+package com.simon.leetcode.list.linkedlist;
+
+import java.util.List;
+
+/**
+ * 将两个同样size的list元素轮流合并为一个新list
+ *
+ * @author zhaoyiwu
+ * @create 2017-12-28 12:46
+ **/
+public class MergeTwoLists {
+
+    /**
+     *  Input: 1->2->4, 1->3->4
+     *  Output: 1->1->2->3->4->4
+     */
+    public void mergeTwoLists(List<Integer> nodeLista,List<Integer> nodeListb){
+
+
+    }
+}
